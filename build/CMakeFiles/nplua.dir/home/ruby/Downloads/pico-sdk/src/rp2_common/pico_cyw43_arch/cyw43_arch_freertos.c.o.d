52b2666bd50176adeabfa0df9774ca8ea90f621d
RubyMaelstrom <kinosian@gmail.com>
2025-11-20 18:58:58 +0000

    Delete build directory

--- build/CMakeFiles/nplua.dir/home/ruby/Downloads/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/nplua.dir/home/ruby/Downloads/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c.o: \
- /home/ruby/Downloads/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c
--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/ruby/Documents/Coding/PicoProj/NPLua/build/_deps/picotool/"
-};
